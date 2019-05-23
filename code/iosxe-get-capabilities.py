a4018e81f46e0a9c4b36d8c74c968af9e0595365
Curtis Smith <curtissm@cisco.com>
2019-05-22 23:41:16 -0400

    Rewriting for consistency with other NETCONF examples

--- code/iosxe-get-capabilities.py
+++ code/iosxe-get-capabilities.py
@@ -21,25 +21,15 @@ or implied.
 from ncclient import manager
 import sys
 
-HOST = '198.18.134.11'
-PORT = 830
-USER = 'netconf'
-PASS = 'C1sco12345'
+m = manager.connect(host="198.18.134.11",
+                    port="830",
+                    username="netconf",
+                    password="C1sco12345",
+                    hostkey_verify=False)
 
+print("This remote network device supports the following capabilities:\n")
 
-def main():
-    # Main method that prints netconf capabilities of remote device.
-    
-    with manager.connect(host=HOST, port=PORT, username=USER,
-                         password=PASS, hostkey_verify=False,
-                         device_params={'name': 'default'},
-                         look_for_keys=False, allow_agent=False) as m:
-        
-        # Print all NETCONF capabilities
-        print('***Here are the Remote Devices Capabilities***')
-        for capability in m.server_capabilities:
-            print(capability.split('?')[0])
+for capability in m.server_capabilities:
+    print(capability.split('?')[0])
 
-
-if __name__ == '__main__':
-    sys.exit(main())
+m.close_session()
