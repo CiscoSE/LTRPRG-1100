ccff59aca2f3c2e026dcec88be38492b3729fa19
Curtis Smith <curtissm@cisco.com>
2019-05-22 23:47:07 -0400

    Fixed errors

--- code/setup_workstation.sh
+++ code/setup_workstation.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-mkdir ~/lab/pythonenv
-python -m venv pythonenv
+mkdir -p ~/lab/pythonenv
+python -m venv ~/lab/pythonenv
 source ~/lab/pythonenv/Scripts/activate
-git clone https://github.com/curtissmith/LTRPRG-1100.git
+git clone https://github.com/curtissmith/LTRPRG-1100.git ~/lab/LTRPRG-1100
 python -m pip install --upgrade pip==19.1.1
 pip install -r ~/lab/LTRPRG-1100/requirements.txt
-git clone https://github.com/YangModels/yang.git
+git clone https://github.com/YangModels/yang.git ~/lab/yang
 pip install pyang==1.7.8
+deactivate
+exit
