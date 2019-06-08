7d1954a12dffb226af212181a9a25f1921d4d470
Curtis Smith <curtissm@cisco.com>
2019-06-08 02:16:35 -0400

    Improvements

--- code/setup_workstation.sh
+++ code/setup_workstation.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+## Download with 'curl https://raw.githubusercontent.com/CiscoSE/LTRPRG-1100/master/code/setup_workstation.sh > setup_workstation.sh'
+## and run with 'source setup_workstation.sh'
+
 mkdir -p ~/lab/pythonenv
+git clone https://github.com/CiscoSE/LTRPRG-1100.git ~/lab/LTRPRG-1100
 python -m venv ~/lab/pythonenv
 source ~/lab/pythonenv/Scripts/activate
 python -m pip install --upgrade pip==19.1.1
 pip install -r ~/lab/LTRPRG-1100/requirements.txt
-git clone https://github.com/YangModels/yang.git ~/lab/yang
 pip install pyang==1.7.8
+git clone https://github.com/YangModels/yang.git ~/lab/yang
 deactivate
-exit
